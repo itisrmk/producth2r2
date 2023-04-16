73f146221f42341541b2ed17aec48e0349be4edf
Rahul Kahsyap <rmkashya@asu.edu>
2023-04-15 17:14:49 -0700

    Update settings.py

--- waterapp/settings.py
+++ waterapp/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 from django.contrib.messages import constants as messages
 import os
 import dj_database_url
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
