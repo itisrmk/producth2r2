0d3d65f93d6acf1262bc3ab26d66a687035b5070
Rahul Kahsyap <rmkashya@asu.edu>
2023-04-18 15:29:15 -0700

    updated smtp elasticmail
    elasticmail smtp server

--- waterapp/settings.py
+++ waterapp/settings.py
@@ -154,8 +154,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST="smtp.sendgrid.net"
-EMAIL_PORT=25
+EMAIL_HOST="smtp.elasticemail.com"
+EMAIL_PORT=2525
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = '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'
+EMAIL_HOST_USER = 'h2r2contact@gmail.com'
+EMAIL_HOST_PASSWORD = '0F24D36584B93256734DA934EE23A4B2A86D'
